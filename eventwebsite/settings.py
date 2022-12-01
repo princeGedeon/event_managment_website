7c0bf57689ee9bf259c6b6d4c98be6444d4e5023
princeGedeon <guedjegedeon03@gmail.com>
2022-12-01 18:21:15 +0100

    custum admin

--- eventwebsite/settings.py
+++ eventwebsite/settings.py
@@ -146,7 +146,7 @@ JAZZMIN_SETTINGS = {
     "site_brand": "IFRI",
 
     # Logo to use for your site, must be present in static files, used for brand on top left
-    "site_logo": "books/img/logo.png",
+    "site_logo": "admin/logo.png",
 
     # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
     "login_logo": None,
@@ -158,17 +158,17 @@ JAZZMIN_SETTINGS = {
     "site_logo_classes": "img-circle",
 
     # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
-    "site_icon": None,
+    "site_icon": "admin/logo.png",
 
     # Welcome text on the login screen
-    "welcome_sign": "Welcome to the library",
+    "welcome_sign": "Bienvenue dans l'espace d'adminitration des evenements à IFRI",
 
     # Copyright on the footer
-    "copyright": "Acme Library Ltd",
+    "copyright": "Developped by PrinceGedeon GUEDJE",
 
     # List of model admins to search from the search bar, search bar omitted if excluded
     # If you want to use a single search field you dont need to use a list, you can use a simple string
-    "search_model": ["auth.User", "auth.Group"],
+    "search_model": ["account.User", "auth.Group"],
 
     # Field name on user model that contains avatar ImageField/URLField/Charfield or a callable that receives the user
     "user_avatar": None,
@@ -203,7 +203,7 @@ JAZZMIN_SETTINGS = {
     # Additional links to include in the user menu on the top right ("app" url type is not allowed)
     "usermenu_links": [
         {"name": "Support", "url": "https://github.com/farridav/django-jazzmin/issues", "new_window": True},
-        {"model": "auth.user"}
+        {"model": "account.user"}
     ],
 
     #############
@@ -217,7 +217,9 @@ JAZZMIN_SETTINGS = {
     "navigation_expanded": True,
 
     # Hide these apps when generating side menu e.g (auth)
-    "hide_apps": [],
+    "hide_apps": [
+
+    ],
 
     # Hide these models when generating side menu (e.g auth.user)
     "hide_models": [],
@@ -226,14 +228,14 @@ JAZZMIN_SETTINGS = {
     "order_with_respect_to": ["auth",],
 
     # Custom links to append to app groups, keyed on app name
-    #"custom_links": {
-     #   "books": [{
-      #      "name": "Make Messages",
-       #     "url": "make_messages",
-        #    "icon": "fas fa-comments",
-         #   "permissions": ["books.view_book"]
-        #}]
-    #}
+    "custom_links": {
+       "books": [{
+           "name": "Make Messages",
+          "url": "make_messages",
+         "icon": "fas fa-comments",
+        "permissions": ["books.view_book"]
+        }]
+    },
 
 
     # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
@@ -242,6 +244,7 @@ JAZZMIN_SETTINGS = {
         "auth": "fas fa-users-cog",
         "auth.user": "fas fa-user",
         "auth.Group": "fas fa-users",
+
     },
     # Icons that are used when one is not manually specified
     "default_icon_parents": "fas fa-chevron-circle-right",
@@ -275,7 +278,7 @@ JAZZMIN_SETTINGS = {
     # - carousel
     "changeform_format": "horizontal_tabs",
     # override change forms on a per modeladmin basis
-    "changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
+    "changeform_format_overrides": {"account.user": "collapsible", "auth.group": "vertical_tabs"},
     # Add a language dropdown into the admin
     "language_chooser": False,
 }
